06153729906734263ad6226251e29d92783526d3
khhini <kiki.h.hutapea@gmail.com>
2025-07-11 11:28:39 +0700

    feat(infra): add cloudbuild trigger

--- deployments/cicd.tf
+++ deployments/cicd.tf
@@ -10,3 +10,21 @@ resource "google_cloudbuildv2_repository" "sample_app" {
   parent_connection = var.cloudbuildv2_github_conn
   remote_uri        = "https://github.com/khhini/sample-pytho-app.git"
 }
+
+resource "google_cloudbuild_trigger" "sample_app" {
+  name            = "sample-app-prd"
+  location        = "asia-east1"
+  project         = var.devops_project_id
+  service_account = "projects/${var.devops_project_id}/serviceAccounts/${var.devops_service_account}"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.sample_app.id
+    push {
+      branch = "main.*"
+    }
+  }
+
+  filename = "deployments/build/cloudbuild.yaml"
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
